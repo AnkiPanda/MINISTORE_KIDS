bb30fb146ccce9bae2ecf5f4ddce88b2ed40c5cc
AnkiPanda <ankitasaha70443@gmail.com>
2024-06-25 13:03:54 +0530

    app  change

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig(({ mode }) => {
       proxy: mode === 'development' ? {
         '/api/v1': 'http://localhost:4000', // Assuming your local backend runs on port 5000
       } : {
-        'api/v1': 'https://ministore-backend.vercel.app'
+        '/api/v1': 'https://ministore-backend.vercel.app'
       },
     },
     build: {
