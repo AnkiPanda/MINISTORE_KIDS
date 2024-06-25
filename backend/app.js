fd9b6aa451b6cd9db38353c7d950f27f4e0b7fcb
AnkiPanda <ankitasaha70443@gmail.com>
2024-06-25 12:12:51 +0530

    app  change

--- backend/app.js
+++ backend/app.js
@@ -66,11 +66,11 @@ app.use("/api/v1",payment)
 
 app.use(cors());
 
-app.use(express.static(path.join(__dirname, "../frontend/dist")));
+// app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "../frontend/dist/index.html"));
-});
+// app.get("*", (req, res) => {
+//   res.sendFile(path.resolve(__dirname, "../frontend/dist/index.html"));
+// });
 
 
 // Middleware for Errors
